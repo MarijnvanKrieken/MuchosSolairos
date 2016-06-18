e609b5a1b472ba0ea193ce02d90a8a4d179b6898
Yorick <yorick_123@hotmail.com>
2016-06-18 22:49:40 +0200

    Gears puzzle partially functional

---
+++ Solairos/Assets/Scripts/DiskManager.cs
@@ -0,0 +1,89 @@
+﻿using DG.Tweening;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiskManager: MonoBehaviour
+{
+	public GameObject innerDisk;
+	public GameObject midDisk;
+	public GameObject outerDisk;
+	public GameObject gear1;
+	public GameObject gear2;
+	public GameObject gear3;
+	public Vector3 gear1Move;
+	public Vector3 gear2Move;
+	public Vector3 gear3Move;
+	public Image cursor;
+	public AudioSource stonesound;
+
+	private Tween gear1Tween;
+	private Tween gear2Tween;
+	private Tween gear3Tween;
+	private CustomDebug customDebug;
+	private bool completed = false;
+	private bool canRotate = true;
+	private float rotateTime = 1f;
+
+	// Use this for initialization
+	void Start()
+	{
+		innerDisk.transform.Rotate(0f, Random.Range(1, 7) * 45f, 0f);
+		midDisk.transform.Rotate(0f, Random.Range(1, 7) * 45f, 0f);
+		outerDisk.transform.Rotate(0f, Random.Range(1, 7) * 45f, 0f);
+		customDebug = FindObjectOfType<CustomDebug>();
+		//customDebug.AddDebug("Start");
+	}
+
+	IEnumerator canRotateTrue()
+	{
+		yield return new WaitForSeconds(rotateTime);
+		canRotate = true;
+		//customDebug.AddDebug("canRotate set to true");
+		yield return null;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 20.0f))
+			{
+				//customDebug.AddDebug("ray cast");
+				if(canRotate)
+				{
+					canRotate = false;
+					stonesound.Play();
+					//customDebug.AddDebug("rotate");
+					if(hit.transform.gameObject.name == gear1.name)
+					{
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear1Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear1Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear1Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+					}
+					else if(hit.transform.gameObject.name == gear2.name)
+					{
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear2Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear2Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear2Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+					}
+					else if(hit.transform.gameObject.name == gear3.name)
+					{
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear3Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear3Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear3Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+					}
+					StartCoroutine("canRotateTrue");
+				}
+			}
+		}
+
+		if(innerDisk.transform.rotation.eulerAngles.y == 0f && midDisk.transform.rotation.eulerAngles.y == 0f && outerDisk.transform.rotation.eulerAngles.y == 0f && completed == false)
+		{
+			customDebug.AddDebug("Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! ");
+			completed = true;
+		}
+	}
+}
--- Solairos/Assets/Scripts/Telekinesis.cs
+++ Solairos/Assets/Scripts/Telekinesis.cs
@@ -1,107 +1,114 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
 
-public class Telekinesis : MonoBehaviour {
+public class Telekinesis: MonoBehaviour
+{
 
-    [SerializeField]
-    Image cursor;
+	[SerializeField]
+	Image cursor;
 
-    GameObject holdingObject = null;
+	GameObject holdingObject = null;
 
 
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start()
+	{
+
 	}
 
-    void CursorColor()
-    {
-        //cursor
-        RaycastHit hitInfo1;
+	void CursorColor()
+	{
+		//cursor
+		RaycastHit hitInfo1;
 
-        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hitInfo1, 200.0f))
-        {
-            //when not holding object
-            if (holdingObject == null)
-            {
-                if (hitInfo1.collider.GetComponent<HoldableObject>() != null)
-                {
-                    cursor.color = Color.black;
+		if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hitInfo1, 200.0f))
+		{
+			//when not holding object
+			if(holdingObject == null)
+			{
+				if(hitInfo1.transform.tag == "Gear")
+				{
+					cursor.color = Color.black;
+					return;
+				}
+				if(hitInfo1.collider.GetComponent<HoldableObject>() != null)
+				{
+					cursor.color = Color.black;
 
-                    return;
-                }
+					return;
+				}
 
-            }
-            else //when holding object
-            {
-                if (hitInfo1.collider.gameObject != holdingObject)
-                {
-                    cursor.color = Color.blue;
-                    return;
-                }
-                    
-            }
-            
-        }
+			}
+			else //when holding object
+			{
+				if(hitInfo1.collider.gameObject != holdingObject)
+				{
+					cursor.color = Color.blue;
+					return;
+				}
 
-        //else
-        cursor.color = Color.white;
-    }
-	
+			}
+
+		}
+
+		//else
+		cursor.color = Color.white;
+	}
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 
 
-        CursorColor();
+		CursorColor();
 
 
-        //on tap
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hitInfo;
+		//on tap
+		if(Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hitInfo;
 
-            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hitInfo, 200.0f))
-            {
-                if (hitInfo.collider.GetComponent<HoldableObject>() && holdingObject == null)
-                {
-                    holdingObject = hitInfo.collider.GetComponent<HoldableObject>().gameObject;
-                    holdingObject.transform.parent = transform.GetChild(0).transform;
+			if(Physics.Raycast(Camera.main.transform.position, Camera.main.transform.TransformDirection(Vector3.forward), out hitInfo, 200.0f))
+			{
+				if(hitInfo.collider.GetComponent<HoldableObject>() && holdingObject == null)
+				{
+					holdingObject = hitInfo.collider.GetComponent<HoldableObject>().gameObject;
+					holdingObject.transform.parent = transform.GetChild(0).transform;
 
-                    holdingObject.GetComponent<HoldableObject>().SendToHold();
-                    
-                }
+					holdingObject.GetComponent<HoldableObject>().SendToHold();
 
-                if (holdingObject != null)
-                {
-                    if (hitInfo.collider.gameObject == holdingObject)
-                        return;
+				}
 
-                    holdingObject.transform.parent = null;
+				if(holdingObject != null)
+				{
+					if(hitInfo.collider.gameObject == holdingObject)
+						return;
 
-                    Vector3 sendToPlace = Vector3.zero;
-                    bool stayAfter = false;
+					holdingObject.transform.parent = null;
 
-                    if (hitInfo.collider.GetComponent<fixedPlace>())
-                    {
-                        hitInfo.collider.GetComponent<fixedPlace>().ReturnGoToPlace(holdingObject.GetComponent<HoldableObject>().objectType, out sendToPlace, out stayAfter);
-                    }
-                    else
-                        sendToPlace = hitInfo.point;
+					Vector3 sendToPlace = Vector3.zero;
+					bool stayAfter = false;
 
-                    
-                    holdingObject.GetComponent<HoldableObject>().SendBack(sendToPlace, stayAfter);
-                    holdingObject = null;
-                }
-
-            }
+					if(hitInfo.collider.GetComponent<fixedPlace>())
+					{
+						hitInfo.collider.GetComponent<fixedPlace>().ReturnGoToPlace(holdingObject.GetComponent<HoldableObject>().objectType, out sendToPlace, out stayAfter);
+					}
+					else
+						sendToPlace = hitInfo.point;
 
 
-        }
-	
+					holdingObject.GetComponent<HoldableObject>().SendBack(sendToPlace, stayAfter);
+					holdingObject = null;
+				}
+
+			}
+
+
+		}
+
 	}
 
 
-    
+
 }
