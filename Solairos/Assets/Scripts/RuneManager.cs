7f57c79a16ac0f9c0523189a2c16e70582670d43
Yorick <yorick_123@hotmail.com>
2016-06-18 14:42:50 +0200

    Added rune manager script and order of notes

---
+++ Solairos/Assets/Scripts/RuneManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class RuneManager: MonoBehaviour
+{
+	public List<GameObject> notes;
+	public List<GameObject> endNotes;
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
