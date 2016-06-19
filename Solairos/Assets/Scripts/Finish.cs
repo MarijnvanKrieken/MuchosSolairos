affbf897bb170fea67b76b70e19dcb227498149b
MarijnvanKrieken <m.vankrieken@hotmail.com>
2016-06-19 15:49:57 +0200

    no message

---
+++ Solairos/Assets/Scripts/Finish.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Finish : MonoBehaviour {
+
+
+    bool mouth = false;
+    bool eyes = false;
+    bool ears = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void CheckBools()
+    {
+        if (mouth == false || eyes == false || ears == false)
+            return;
+
+        
+    }
+
+
+
+    public void MouthDone()
+    {
+        mouth = true;
+        CheckBools();
+    }
+
+    public void EyesDone()
+    {
+        eyes = true;
+        CheckBools();
+    }
+
+    public void EarsDone()
+    {
+        ears = true;
+        CheckBools();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
