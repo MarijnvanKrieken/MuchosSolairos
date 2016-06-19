9c7cd5a83a6f38cfddf434ac43432f9ec9aff517
Yorick <yorick_123@hotmail.com>
2016-06-19 13:41:06 +0200

    Updated scene models, added scene textures to assets folder, added dust particles to assets folder, modified scripts to match new orientation of rotating objects, made ears fall when playing the large bell

--- Solairos/Assets/Scripts/DiskManager.cs
+++ Solairos/Assets/Scripts/DiskManager.cs
@@ -16,7 +16,7 @@ public class DiskManager: MonoBehaviour
 	public Vector3 gear3Move;
 	public Image cursor;
 	public AudioSource stonesound;
-    public GameObject Banana;
+	public GameObject Banana;
 
 	private Tween gear1Tween;
 	private Tween gear2Tween;
@@ -29,15 +29,15 @@ public class DiskManager: MonoBehaviour
 	// Use this for initialization
 	void Start()
 	{
-        Banana.SetActive(false);
+		Banana.SetActive(false);
 		customDebug = FindObjectOfType<CustomDebug>();
 		//customDebug.AddDebug("start disks rotations: " + innerDisk.transform.localRotation.eulerAngles.y.ToString() + "," + midDisk.transform.localRotation.eulerAngles.y.ToString() + "," + outerDisk.transform.localRotation.eulerAngles.y.ToString());
-		while(innerDisk.transform.localRotation.eulerAngles.y == 0f)
-			innerDisk.transform.localRotation = Quaternion.Euler(0f, Random.Range(0, 7) * 45f, 0f);
-		while(midDisk.transform.localRotation.eulerAngles.y == 0f)
-			midDisk.transform.localRotation = Quaternion.Euler(0f, Random.Range(0, 7) * 45f, 0f);
-		while(outerDisk.transform.localRotation.eulerAngles.y == 0f)
-			outerDisk.transform.localRotation = Quaternion.Euler(0f, Random.Range(0, 7) * 45f, 0f);
+		while(innerDisk.transform.localRotation.eulerAngles.x == 0f)
+			innerDisk.transform.localRotation = Quaternion.Euler(Random.Range(0, 7) * 45f, 0f, 0f);
+		while(midDisk.transform.localRotation.eulerAngles.x == 0f)
+			midDisk.transform.localRotation = Quaternion.Euler(Random.Range(0, 7) * 45f, 0f, 0f);
+		while(outerDisk.transform.localRotation.eulerAngles.x == 0f)
+			outerDisk.transform.localRotation = Quaternion.Euler(Random.Range(0, 7) * 45f, 0f, 0f);
 		//customDebug.AddDebug("initialized disks rotations: " + innerDisk.transform.localRotation.eulerAngles.y.ToString() + "," + midDisk.transform.localRotation.eulerAngles.y.ToString() + "," + outerDisk.transform.localRotation.eulerAngles.y.ToString());
 		//customDebug.AddDebug("Start");
 	}
@@ -46,9 +46,9 @@ public class DiskManager: MonoBehaviour
 	{
 		yield return new WaitForSeconds(rotateTime);
 		canRotate = true;
-		innerDisk.transform.localRotation = Quaternion.Euler(innerDisk.transform.localRotation.eulerAngles.x, Mathf.Round(innerDisk.transform.localRotation.eulerAngles.y), innerDisk.transform.localRotation.eulerAngles.z);
-		midDisk.transform.localRotation = Quaternion.Euler(midDisk.transform.localRotation.eulerAngles.x, Mathf.Round(midDisk.transform.localRotation.eulerAngles.y), midDisk.transform.localRotation.eulerAngles.z);
-		outerDisk.transform.localRotation = Quaternion.Euler(outerDisk.transform.localRotation.eulerAngles.x, Mathf.Round(outerDisk.transform.localRotation.eulerAngles.y), outerDisk.transform.localRotation.eulerAngles.z);
+		innerDisk.transform.localRotation = Quaternion.Euler(Mathf.Round(innerDisk.transform.localRotation.eulerAngles.x), innerDisk.transform.localRotation.eulerAngles.y, innerDisk.transform.localRotation.eulerAngles.z);
+		midDisk.transform.localRotation = Quaternion.Euler(Mathf.Round(midDisk.transform.localRotation.eulerAngles.x), midDisk.transform.localRotation.eulerAngles.y, midDisk.transform.localRotation.eulerAngles.z);
+		outerDisk.transform.localRotation = Quaternion.Euler(Mathf.Round(outerDisk.transform.localRotation.eulerAngles.x), outerDisk.transform.localRotation.eulerAngles.y, outerDisk.transform.localRotation.eulerAngles.z);
 		//customDebug.AddDebug("new disks rotations: " + innerDisk.transform.localRotation.eulerAngles.y.ToString() + "," + midDisk.transform.localRotation.eulerAngles.y.ToString() + "," + outerDisk.transform.localRotation.eulerAngles.y.ToString());
 		yield return null;
 	}
@@ -70,27 +70,27 @@ public class DiskManager: MonoBehaviour
 					{
 						gear1.transform.DORotate(new Vector3(321f, 0f, 0f), rotateTime * 0.8f, RotateMode.LocalAxisAdd);
 
-						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear1Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear1Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear1Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(gear1Move.x * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(gear1Move.y * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(gear1Move.z * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
 						startCanRotate = true;
 					}
 					else if(hit.transform.gameObject.name == gear2.name)
 					{
 						gear2.transform.DORotate(new Vector3(297f, 0f, 0f), rotateTime * 0.8f, RotateMode.LocalAxisAdd);
 
-						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear2Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear2Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear2Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(gear2Move.x * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(gear2Move.y * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(gear2Move.z * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
 						startCanRotate = true;
 					}
 					else if(hit.transform.gameObject.name == gear3.name)
 					{
 						gear3.transform.DORotate(new Vector3(382f, 0f, 0f), rotateTime * 0.8f, RotateMode.LocalAxisAdd);
 
-						gear1Tween = innerDisk.transform.DORotate(new Vector3(0f, gear3Move.x * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear2Tween = midDisk.transform.DORotate(new Vector3(0f, gear3Move.y * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
-						gear3Tween = outerDisk.transform.DORotate(new Vector3(0f, gear3Move.z * 45f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear1Tween = innerDisk.transform.DORotate(new Vector3(gear3Move.x * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear2Tween = midDisk.transform.DORotate(new Vector3(gear3Move.y * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
+						gear3Tween = outerDisk.transform.DORotate(new Vector3(gear3Move.z * 45f, 0f, 0f), rotateTime, RotateMode.LocalAxisAdd);
 						startCanRotate = true;
 					}
 					if(startCanRotate)
@@ -103,10 +103,10 @@ public class DiskManager: MonoBehaviour
 			}
 		}
 
-		if(innerDisk.transform.localRotation.eulerAngles.y == 0f && midDisk.transform.localRotation.eulerAngles.y == 0f && outerDisk.transform.localRotation.eulerAngles.y == 0f && completed == false)
+		if(innerDisk.transform.localRotation.eulerAngles.x == 0f && midDisk.transform.localRotation.eulerAngles.x == 0f && outerDisk.transform.localRotation.eulerAngles.x == 0f && completed == false)
 		{
-            Banana.SetActive(true);
-            //customDebug.AddDebug("Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! ");
+			Banana.SetActive(true);
+			//customDebug.AddDebug("Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! Gears complete! ");
 			completed = true;
 		}
 	}
